e316bff6e09b8534dda94edb732d46b75718510c
pharmeasyMarketing <134919459+pharmeasyMarketing@users.noreply.github.com>
2023-05-30 11:17:07 +0530

    Update script.py

--- script.py
+++ script.py
@@ -550,19 +550,19 @@ def main():
     st.header("Current Features")
     st.markdown("""
 
-Scrapes the top 10 search results and creates SEO semantics using NLP.
-Sends the SEO semantics to GPT-3.5 to generate an outline based on the semantics.
-Improves the generated outline with the required sections.
-Uses GPT-3.5 to write the article based on the improved sections.
-After generating the article, it further improves the content and creates the final draft.
+* Scrapes the top 10 search results and creates SEO semantics using NLP.
+* Sends the SEO semantics to GPT-3.5 to generate an outline based on the semantics.
+* Improves the generated outline with the required sections.
+* Uses GPT-3.5 to write the article based on the improved sections.
+* After generating the article, it further improves the content and creates the final draft.
 """)
     st.header("Upcoming Features")
     st.markdown("""
 
-References at the end of the article.
-Option to define the desired word count for the article.
-Automatic draft saving in WordPress.
-Top 5 FAQs from "People Also Ask" section.
+* References at the end of the article.
+* Option to define the desired word count for the article.
+* Automatic draft saving in WordPress.
+* Top 5 FAQs from "People Also Ask" section.
 """)
 
     topic = st.text_input("Enter topic:")
