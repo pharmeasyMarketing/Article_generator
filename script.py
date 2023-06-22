c3ec54820e06d21b1e8330b9069e8a9323894761
pharmeasyMarketing <134919459+pharmeasyMarketing@users.noreply.github.com>
2023-06-22 18:41:39 +0530

    Update script.py

--- script.py
+++ script.py
@@ -610,7 +610,7 @@ def generate_semantic_improvements_guide(
 
     # return outline
 
-def generate_outline(topic, Introduction_conclusion_input, model="text-davinci-003", max_tokens=1000, temperature=1):
+def generate_outline(topic, Introduction_conclusion_input, model="text-davinci-003", max_tokens=1000, temperature=0.8):
     model = "text-davinci-003"
     # prompt = f"Generate a purposeful outline with intent-focused headings and subheadings, structured with proper Roman numerals, Roman numerals should be aligned well, for the topic: {topic}. The outline should effectively align with the intended scope of the topic. Utilize a hierarchical organization for the headings and subheadings, maintaining clarity and coherence. You may incorporate up to 10 levels of subheadings, depending on the topic's complexity, but only if necessary. Additionally, include an introductory section and a conclusion as headings, without subheadings, while ensuring they remain within the intended scope of the topic without providing detailed explanations."
     # prompt = f"Generate a purposeful outline with intent-focused headings and subheadings, structured with proper Roman numerals, Roman numerals should be aligned well, for the topic: {topic}. The outline should effectively align with the intended scope of the topic. Utilize a hierarchical organization for the headings and subheadings, maintaining clarity and coherence. Please please please strictly Avoid including the Introduction, overview and conclusion in the outline, ensuring that they are not part of the layout and never add them while generating the outline."
@@ -619,7 +619,7 @@ def generate_outline(topic, Introduction_conclusion_input, model="text-davinci-0
         engine=model,
         prompt=prompt,
         max_tokens=max_tokens,
-        temperature=1,
+        temperature=0.8,
         n=1,
         stop=None,
     )
